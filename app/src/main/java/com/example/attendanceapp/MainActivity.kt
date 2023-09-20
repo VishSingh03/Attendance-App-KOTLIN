582bd1827c714b4340f5c40a3d6dc7bda313487d
VishSingh03 <vishalrajpurohit603@gmail.com>
2023-09-20 18:15:07 +0530

    first commit

---
+++ app/src/main/java/com/example/attendanceapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.attendanceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
